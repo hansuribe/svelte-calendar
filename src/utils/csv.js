cfe23928c6f2315a38f86cdfcad0110a8d53834f
Owen Granot <granotowen@gmail.com>
2020-10-14 10:28:56 +0300

    Added CSV functionality - can download all days

--- src/utils/csv.js
+++ src/utils/csv.js
@@ -1,31 +1,39 @@
 const data = JSON.parse(localStorage.getItem("data"));
-const columnDelimiter = ',';
-const lineDelimiter = '\n';
+const columnDelimiter = ",";
+const lineDelimiter = "\n";
 
 let days = [];
 
 // [{name, hours: { desc, bg }}]
 
 export default function exportToCsv() {
-    console.log(data);
-    if (data == null || !data.length)
-        return null;
-    
-    data.forEach((day) => createDayData(day));
+  if (data == null || !data.length) return null;
+
+  data.forEach((day) => createDayData(day));
+
+  let i = 0;
+  days.forEach((dayCsv) => {
+    dayCsv = "data:text/csv;charset=utf-8," + dayCsv;
+
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = dayCsv;
+    a.download = data[i++].name + '.csv';
+    a.click();
+  });
 }
 
 function createDayData(day) {
-    const hoursKeys = Object.keys(day.hours);
-    let result = '';
+  const hoursKeys = Object.keys(day.hours);
+  let result = "";
 
-    result += ['hour','desc'].join(columnDelimiter);
+  result += ["hour", "desc"].join(columnDelimiter);
+  result += lineDelimiter;
+
+  hoursKeys.forEach((hour) => {
+    result += [hour, day.hours[hour].desc].join(columnDelimiter);
     result += lineDelimiter;
+  });
 
-    hoursKeys.forEach((hour) => {
-        result += [hour, day.hours[hour].desc].join(columnDelimiter);
-        result += lineDelimiter;
-    })
-
-    days.push(result);
-    console.log(result);
+  days.push(result);
 }
